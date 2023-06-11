658738ce510232ad2b7315a4333c532af1471cd1
Docenko Egor <egordocenko369@gmail.com>
2023-06-11 15:31:06 +0300

    улутшили посты

--- src/components/UI/Post/Post.jsx
+++ src/components/UI/Post/Post.jsx
@@ -1,28 +1,55 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import styles from './Post.module.css'
-const Post = ({data}) => {
-const {id,title,body,isHaveImg,like,reposts,isCommented} = data;
-const [likeV,setLike] = useState(like);
-const [repostsV,setRepostsV] = useState(reposts);
-const [comments,setComments] = useState(false);
+const Post = ({ data }) => {
+  const { id, title, body, isHaveImg, like, reposts, isCommented, disLikes } = data;
+  const [isLiked, setLiked] = useState(0)
   return (
     <div className={styles.wrapper}>
-        <h1>{title}</h1>
-        <p>{body}</p>
-        <div className={styles.footerPost}>репосты:{repostsV}</div>
-        <div className={styles.footerPost}>лайки:{likeV}</div>
-        <button onClick={(e) => 
-        {
-            e.preventDefault()
-            setLike(likeV+1)
-        }
-        }>лайк</button>
-        <button onClick={(e) => 
-        {
-            e.preventDefault()
-            setLike(likeV-1)
-        }
-        }>дизлайк</button>
+      <div className={styles.header_post}>
+
+        <div className={styles.title}>
+
+          {title}
+
+        </div>
+
+        <div className={styles.body}>
+
+          {body}
+
+        </div>
+
+      </div>
+      <div className={styles.footer_post}>
+        <div className={styles.widget_wrapper}>
+          <div className="#">10</div>
+          <button>r</button>
+        </div>
+
+        <div className={styles.widget_wrapper}>
+          <div className="#">{135 + isLiked}</div>
+          <button onClick={
+            () => {
+              if(isLiked) setLiked(0)
+
+              else(setLiked(1))
+
+              
+            }
+          }>l</button>
+        </div>
+
+        <div className={styles.widget_wrapper}>
+          <div className="#">2</div>
+          <button>d</button>
+        </div>
+
+        <div className={styles.widget_wrapper}>
+          <div className="#">65</div>
+          <button>c</button>
+        </div>
+
+      </div>
     </div>
   )
 }
